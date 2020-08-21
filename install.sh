301348ffa4d83831d87bda741698309e82b83ce2
IntrovertedCoder <natrotnic@gmail.com>
2020-08-21 05:26:58 +0100

    Added fast install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+mv ~/.vimrc ~/.vimrc.bah
+mv ~/.vim ~/.vimrc.bak
+mv ~/.bashrc ~/.bashrc.bak
+ln -s ~/.config/dots/vim/vimrc ~/.vimrc
+ln -s ~/.config/dots/vim/vim ~/.vim
+ln -s ~/.config/dots/bash/bashrc ~/.bashrc
+ln -s ~/.config/dots/bash/bash_aliases ~/.bash_aliases
+sudo ln -s ~/.config/dots/bin/googler /bin/googler
