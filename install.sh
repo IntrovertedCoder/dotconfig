9fb0239964269e4a48eabafab78324202f2755a4
IntrovertedCoder <natrotnic@gmail.com>
2020-08-22 05:01:56 +0100

    Added ssh config, setting editor, and installing wget

--- install.sh
+++ install.sh
@@ -3,10 +3,15 @@
 mv ~/.vimrc ~/.vimrc.bah
 mv ~/.vim ~/.vimrc.bak
 mv ~/.bashrc ~/.bashrc.bak
+
 ln -s ~/.config/dots/vim/vimrc ~/.vimrc
 ln -s ~/.config/dots/vim/vim ~/.vim
 ln -s ~/.config/dots/bash/bashrc ~/.bashrc
 ln -s ~/.config/dots/bash/bash_aliases ~/.bash_aliases
+sudo ln -s ~/.config/dots/ssh/sshd_config /etc/ssh/sshd_config
 sudo ln -s ~/.config/dots/bin/googler /bin/googler
 
+export EDITOR=/usr/bin/vim
+
 sudo apt-get install tree
+sudo apt-get install wget
