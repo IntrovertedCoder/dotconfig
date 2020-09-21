461056e5e50862648eb6f169cb264db983ad4b99
IntrovertedCoder <natrotnic@gmail.com>
2020-09-21 06:02:42 +0100

    Added fish shell config symbiotic links

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ ln -s ~/.config/dots/vim/vimrc ~/.vimrc
 ln -s ~/.config/dots/vim/vim ~/.vim
 ln -s ~/.config/dots/bash/bashrc ~/.bashrc
 ln -s ~/.config/dots/bash/bash_aliases ~/.bash_aliases
+mkdir ~/.config/fish
+ln -s ~/.config/dots/fish/config.fish ~/.config/fish/config.fish
+ln -s ~/.config/dots/fish/fish_variables ~/.config/fish/fish_variables
+ln -s ~/.config/dots/fish/functions/ ~/.config/fish/functions
 sudo ln -s ~/.config/dots/ssh/sshd_config /etc/ssh/sshd_config
 sudo ln -s ~/.config/dots/bin/googler /bin/googler
 
