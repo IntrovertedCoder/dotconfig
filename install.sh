af3f44c77e7262bfb12589c563a6639aa9a7fc7f
IntrovertedCoder <natrotnic@gmail.com>
2020-10-26 18:58:49 -0500

    Started working on an install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+rm ~/.config/fish
+ln -s ~/.config/dots/fish/ ~/.config/fish
+rm ~/.vim
+ln -s ~/.config/dots/vim/vim/ ~/.vim
+rm ~/.vimrc
+ln -s ~/.config/dots/vim/vimrc ~/.vimrc
+
+sudo ln -s ~/.config/dots/bin/googler /bin/googler
+
+sudo apt install tree
+sudo apt install wget
