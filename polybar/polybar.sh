e8b878fc356a405e271873592ec33bc199fe4814
IntrovertedCoder <NATrotnic@gmail.com>
2022-03-24 14:43:37 -0400

    Fixed errors showing up in console

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/sh
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload example 2> /dev/null &
   done
 else
   polybar --reload example &
